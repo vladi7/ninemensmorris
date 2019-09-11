9a2e326973988e1664229698dc8225804c434cf5
vladi7 <dubrovenski.v@gmail.com>
2019-09-11 11:21:16 -0500

    Board and GUI classes created

---
+++ src/sprint1/product/Board.java
@@ -0,0 +1,6 @@
+package sprint1.product;
+
+
+public class Board {
+	
+}
---
+++ src/sprint1/product/GUI.java
@@ -0,0 +1,9 @@
+package sprint1.product;
+import java.awt.*;
+
+import javax.swing.*;
+
+import sprint1.product.Board;
+public class GUI {
+
+}
