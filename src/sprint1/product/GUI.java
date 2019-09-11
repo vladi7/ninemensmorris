84a03c6e3e7b44694f025be79f54b01a56728450
vladi7 <dubrovenski.v@gmail.com>
2019-09-11 13:37:47 -0500

    Supressing Warning Vlad

--- src/sprint1/product/GUI.java
+++ src/sprint1/product/GUI.java
@@ -1,9 +1,11 @@
 package sprint1.product;
+
 import java.awt.*;
 
 import javax.swing.*;
-
 import sprint1.product.Board;
-public class GUI {
+
+@SuppressWarnings("serial")
+public class GUI extends JFrame {
 
 }
