9b56dfdf47d6752176a8938fae4ef4bfe4934efc
matth <matth@DESKTOP-C6DRFMJ>
2019-10-29 14:26:36 -0500

    Tests highlights

---
+++ src/sprint2/test/TestAvailableMoves.java
@@ -0,0 +1,42 @@
+package sprint2.test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import sprint2.product.Board;
+import sprint2.product.Dot;
+import sprint2.product.GUI;
+
+
+public class TestAvailableMoves {
+	private final Board board = new Board(4);
+
+	@Test
+	public void testMovesAreAvailable() {
+		board.makeMoveFirstPhase(13);
+		board.makeMoveFirstPhase(1);
+		board.makeMoveFirstPhase(2);
+		board.makeMoveFirstPhase(3);
+		board.makeMoveFirstPhase(4);
+		board.makeMoveFirstPhase(5);
+		board.makeMoveFirstPhase(6);
+		board.makeMoveFirstPhase(7);
+		board.makeMoveSecondPhaseA(13);
+
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertThat(board.getDot(5,3), is(Dot.GRAY));
+		assertThat(board.getDot(4,3), is(Dot.HIGHLIGHT));
+		assertThat(board.getDot(6,3), is(Dot.HIGHLIGHT));
+		assertThat(board.getDot(5,1), is(Dot.BLACK));
+		assertThat(board.getDot(5,5), is(Dot.HIGHLIGHT));
+	}
+
+}
