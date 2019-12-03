cdf21ab03c86a8bb4170c17e3eaa0303c6b04389
matth <matth@DESKTOP-C6DRFMJ>
2019-12-03 13:54:36 -0600

    win conditions

--- src/sprint2/test/TestWinConditions.java
+++ src/sprint2/test/TestWinConditions.java
@@ -29,6 +29,13 @@ public class TestWinConditions {
 		board.makeMoveFirstPhase(16);
 		board.makeMoveFirstPhase(17);
 		board.makeMoveFirstPhase(12);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertEquals(board.getGameState(),GameState.BLACK_WON);
 
 	}
@@ -45,7 +52,13 @@ public class TestWinConditions {
 		board.makeMoveFirstPhase(15);
 		board.makeMoveFirstPhase(13);
 		board.makeMoveFirstPhase(17);
-		
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		board.makeMoveSecondPhaseA(13);
 		board.makeMoveSecondPhaseB(13, 12);
@@ -57,5 +70,180 @@ public class TestWinConditions {
 			e.printStackTrace();
 		}
 		assertEquals(board.getGameState(),GameState.WHITE_WON);
+	}
+	
+	@Test
+	public void testWhiteOpponentLessThanThreePiecesBlackWins() {
+		board.setCurrentGameRegime(GameRegime.P1vP2);
+		board.setGameState(GameState.PLAYING1);
+		board.setCurrentTurn(Dot.WHITE);
+		
+		board.makeMoveFirstPhase(0);
+		board.makeMoveFirstPhase(9);
+		board.makeMoveFirstPhase(1);
+		board.makeMoveFirstPhase(21);
+		board.makeMoveFirstPhase(14);
+		board.makeMoveFirstPhase(15);
+		board.makeMoveFirstPhase(23);
+		board.makeMoveFirstPhase(22);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(14);
+		board.makeMoveSecondPhaseB(14, 2);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveThirdPhase(21);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(22);
+		board.makeMoveSecondPhaseB(22, 21);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(2);
+		board.makeMoveSecondPhaseB(2, 14);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(21);
+		board.makeMoveSecondPhaseB(21, 22);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(14);
+		board.makeMoveSecondPhaseB(14, 2);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveThirdPhase(22);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(board.getGameState(),GameState.WHITE_WON);
+		
+
+
+	}
+	@Test
+	public void testBlackOpponentLessThanThreePiecesWhiteWins() {
+		board.setCurrentGameRegime(GameRegime.P1vP2);
+		board.setGameState(GameState.PLAYING1);
+		board.setCurrentTurn(Dot.BLACK);
+		
+		board.makeMoveFirstPhase(0);
+		board.makeMoveFirstPhase(9);
+		board.makeMoveFirstPhase(1);
+		board.makeMoveFirstPhase(21);
+		board.makeMoveFirstPhase(14);
+		board.makeMoveFirstPhase(15);
+		board.makeMoveFirstPhase(23);
+		board.makeMoveFirstPhase(22);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(14);
+		board.makeMoveSecondPhaseB(14, 2);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveThirdPhase(21);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(22);
+		board.makeMoveSecondPhaseB(22, 21);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(2);
+		board.makeMoveSecondPhaseB(2, 14);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(21);
+		board.makeMoveSecondPhaseB(21, 22);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveSecondPhaseA(14);
+		board.makeMoveSecondPhaseB(14, 2);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		board.makeMoveThirdPhase(22);
+		new GUI(board); 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(board.getGameState(),GameState.BLACK_WON);
+		
+
+
 	}
 }
